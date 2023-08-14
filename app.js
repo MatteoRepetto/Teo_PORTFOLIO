9d5304d763497dd98c48cf967e7e624a407d3018
MatteoRepetto <matteo.repetto@mail.polimi.it>
2023-08-14 12:12:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -138,13 +138,13 @@ document.getElementById("colored__img9").addEventListener('mouseout', (event) =>
 //   document.getElementById("colored__img11").src = '/images/12_growmate/00_cover.png'
 // })
 
-document.getElementById("colored_img11").addEventListener('mouseover', (event) => {
-  document.getElementById("colored_img11").src = '/images/11_Robbin/00_cover2.png'
+document.getElementById("colored_img12").addEventListener('mouseover', (event) => {
+  document.getElementById("colored_img12").src = '/images/11_Robbin/00_cover2.png'
   console.log('j')
 })
 
-document.getElementById("colored__img11").addEventListener('mouseout', (event) => {
-  document.getElementById("colored__img11").src = '/images/11_Robbin/00_cover.png'
+document.getElementById("colored__img12").addEventListener('mouseout', (event) => {
+  document.getElementById("colored__img12").src = '/images/11_Robbin/00_cover.png'
 
 })
 
