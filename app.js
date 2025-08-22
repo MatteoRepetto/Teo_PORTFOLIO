ba437976133d626b8ba6b81377ec5a758a34d5eb
MatteoRepetto <matteo.repetto@mail.polimi.it>
2025-08-22 15:17:32 +0200

    Added kaifu UX UI

--- app.js
+++ app.js
@@ -202,21 +202,21 @@ document.getElementById("colored__img9").addEventListener('touchend', (event) =>
 })*/
 
 document.getElementById("colored__img11").addEventListener('mouseover', (event) => {
-  document.getElementById("colored__img11").src = './images/10_Cards/00_cover2.png'
+  document.getElementById("colored__img11").src = './images/17_Kaifu/00_cover2.png'
   console.log('j')
 })
 
 document.getElementById("colored__img11").addEventListener('mouseout', (event) => {
-  document.getElementById("colored__img11").src = './images/10_Cards/00_cover.png'
+  document.getElementById("colored__img11").src = './images/17_Kaifu/00_cover.png'
 })
 
 document.getElementById("colored__img11").addEventListener('touchstart', (event) => {
-  document.getElementById("colored__img11").src = './images/10_Cards/00_cover2.png'
+  document.getElementById("colored__img11").src = './images/17_Cards/00_cover2.png'
   console.log('j')
 })
 
 document.getElementById("colored__img11").addEventListener('touchend', (event) => {
-  document.getElementById("colored__img11").src = './images/10_Cards/00_cover.png'
+  document.getElementById("colored__img11").src = './images/17_Kaifu/00_cover.png'
 })
 
 document.getElementById("colored__img12").addEventListener('mouseover', (event) => {
